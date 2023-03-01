3ea5eed8eee253afb129e95261d10a993095f008
Rudhi Mediastara <rudhi.mediastara@gmail.com>
2023-03-01 16:50:58 +0700

    add volta script to fish

--- flake.nix
+++ flake.nix
@@ -66,6 +66,10 @@
             
             programs.fish = {
                enable = true;
+               loginShellInit = "
+               export VOLTA_HOME=$HOME/.volta
+               export PATH=$VOLTA_HOME/bin:$PATH
+               ";
             };
             
 
